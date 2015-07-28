1da6e4b06738c0384057f7ded72a83fa187ac8d3
Trey Huffine <treyhuffine@gmail.com>
2015-07-28 14:11:07 -0700

    Display hello world component

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Router, Route, Link} from 'react-router';
+import { history } from 'react-router/lib/HashHistory';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
